4f4863e4e3c1c7e01df44d65c8f00afd1a06c172
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-16 19:18:28 +0200

    feat: access control header

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ app.use(express.urlencoded({
     extended: false
 }));
 
+app.use(
+    (req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    }
+)
+
 app.use("/", router);
 app.use("/cardpacks", require("./routes/cardpack"));
 
